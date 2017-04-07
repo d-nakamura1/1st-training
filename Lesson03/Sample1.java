a0c0371781eacf324ea9e1c708b7075fa017d7a8
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-04-07 17:45:20 +0900

    Leeson03 Sample1 修正

--- Lesson03/Sample1.java
+++ Lesson03/Sample1.java
@@ -2,7 +2,7 @@ class Sample1 {
    public static void main(String[] args) {
       int num;
 
-      num = 2147483648;
+      num = 2147483647;
       System.out.println("変数numの値は\""+ num +"\"です。");
    }
 }
