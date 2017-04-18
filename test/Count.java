c6f52f47c9ed9fbd5c8e82bc81b7fa94aed83e20
Daiki-N <dakidakidaiki@yahoo.co.jp>
2017-04-18 23:15:28 +0900

    Count.java 20170418
    Count.javaの修正

--- test/Count.java
+++ test/Count.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Count {
    public static void main(String[] args) throws IOException{
@@ -15,24 +17,29 @@ class Count {
          target[i] = set;
          set++;
       }
-      set = 'A'
+      set = 'A';
       for(int i = 26; i < 52; i++) {
          target[i] = set;
          set++;
       }
 
+      int[] num = new int[52];
 
-      for(i = 0; i < 52; i++) {
-         for(){
-            if(x == target){
+      for(int i = 0; i < 52; i++) {
+         for(int j = 0; j < str.length(); j++) {
+            char x = str.charAt(j);
+            if(x == target[i]) {
                count++;
             }
          }
-         if(count > 0) {
-            System.out.println(target + "" + count);
-         }
-         target++;
+         num[i] = count;
          count = 0;
+      }
+
+      for(int i = 0; i < target.length; i++) {
+         if(num[i] > 0) {
+            System.out.println(target[i] + "" + num[i]);
+         }
       }
    }
 }
